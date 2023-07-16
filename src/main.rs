ad1f271aaa562f0242924469fae2b06051684969
Eugene Marcotte <emarcotte@gmail.com>
2023-07-16 12:20:52 -0400

    feat: Some initial back tracking logic, full of clones.

--- src/main.rs
+++ src/main.rs
@@ -1,188 +1,24 @@
 use std::collections::HashMap;
-use std::fmt::Display;
 
 use csp::ConstraintProvider;
-use rand::prelude::SmallRng;
-use rand::seq::SliceRandom;
+use rand::{prelude::SmallRng, seq::SliceRandom};
 use rand_seeder::Seeder;
+use tile_matcher::{Coordinate, TileSet};
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-/// Define a position on the 2d map.
-/// Assume that **Down the screen** is lower Y -- The bottom of the screen y = 0.
-#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
-pub struct Coordinate(usize, usize);
-
-impl Coordinate {
-    /// Returns the direction `other` is in relation to `self`.
-    pub fn is_adjacent(&self, other: &Self) -> Option<Direction> {
-        let x_adjacent =
-            (self.0 > 0 && self.0 - 1 == other.0) || (self.0 < usize::MAX && self.0 + 1 == other.0);
-        let y_adjacent =
-            (self.1 > 0 && self.1 - 1 == other.1) || (self.1 < usize::MAX && self.1 + 1 == other.1);
-
-        if !(x_adjacent ^ y_adjacent) {
-            None
-        } else if self.0 < other.0 {
-            Some(Direction::Right)
-        } else if self.0 > other.0 {
-            Some(Direction::Left)
-        } else if self.1 < other.1 {
-            Some(Direction::Up)
-        } else {
-            Some(Direction::Down)
-        }
-    }
-}
-
-/// Collection of tiles and valid relationships to each other.
-pub struct TileSet {
-    /// Visual reference for what each tile is.
-    pub tiles: Vec<char>,
-
-    // Tile index relation to other tile indexes by direction. E.g. `tiles`
-    // index 0 is relations index 0, and has `[up, down, left, right]` where `up`
-    // is a vec of indexes into `tiles`.
-    pub relations: Vec<[Vec<usize>; 4]>,
-}
-
-fn new_relation(
-    up: Vec<usize>,
-    down: Vec<usize>,
-    left: Vec<usize>,
-    right: Vec<usize>,
-) -> [Vec<usize>; 4] {
-    [up, down, left, right]
-}
-
-impl Default for TileSet {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl TileSet {
-    pub fn new() -> Self {
-        // Tiles:
-        // ░
-        // ┌─┐
-        // │▓│
-        // └─┘
-        //                           0    1    2    3    4    5    6    7
-        let tiles: Vec<char> = vec!['░', '┌', '─', '┐', '│', '▓', '└', '┘'];
-        // up down left right (where "up" means "this tile is below the provided one")
-        let relations = vec![
-            // 0
-            new_relation(
-                vec![0, 2, 6, 7],
-                vec![0, 1, 2, 3],
-                vec![0, 3, 4, 7],
-                vec![0, 1, 4, 6],
-            ),
-            // 1 ┌
-            new_relation(vec![0, 2, 6, 7], vec![4], vec![0, 4, 3, 7], vec![2]),
-            // 2 ─
-            new_relation(vec![0, 2, 6, 7], vec![2, 5], vec![1, 6, 2], vec![2, 3, 7]),
-            // 3 ┐
-            new_relation(vec![0, 2, 6, 7], vec![4], vec![2], vec![0, 1, 4, 6]),
-            // 4 │
-            new_relation(vec![1, 3, 4], vec![4, 6, 7], vec![0, 5], vec![0, 5]),
-            // 5 ▓
-            new_relation(vec![2, 5], vec![2, 5], vec![4, 5], vec![4, 5]),
-            // 6 └
-            new_relation(vec![4], vec![0, 1, 2, 3], vec![0, 3, 4, 7], vec![2]),
-            // 7 ┘
-            new_relation(vec![4], vec![0, 1, 2, 3], vec![2], vec![]),
-        ];
-
-        Self { tiles, relations }
-    }
-
-    fn debug(&self) {
-        for (i, t) in self.tiles.iter().enumerate() {
-            let rels = &self.relations[i];
-            println!("Tile {i}: {t}");
-            let up = &rels[0];
-            let down = &rels[1];
-            let left = &rels[2];
-            let right = &rels[3];
-            for o in up.iter() {
-                println!(" up    {}", &self.tiles[*o]);
-                println!("       {t}");
-                println!("     xxx")
-            }
-            for o in down.iter() {
-                println!("       {t}");
-                println!(" down  {}", &self.tiles[*o]);
-                println!("     xxx")
-            }
-            for o in left.iter() {
-                println!(" left  {}{t}", &self.tiles[*o]);
-                println!("     xxx")
-            }
-            for o in right.iter() {
-                println!(" right {t}{}", &self.tiles[*o]);
-                println!("     xxx")
-            }
-        }
-    }
-}
-
-// up down left right
-fn get_relation_index(dir: &Direction) -> usize {
-    match dir {
-        Direction::Up => 0,
-        Direction::Down => 1,
-        Direction::Left => 2,
-        Direction::Right => 3,
-    }
-}
-
-impl Display for Coordinate {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("({}, {})", self.0, self.1))
-    }
-}
-
-impl Display for Direction {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Direction::Up => f.write_str("up"),
-            Direction::Down => f.write_str("down"),
-            Direction::Left => f.write_str("left"),
-            Direction::Right => f.write_str("right"),
-        }
-    }
-}
-
-impl ConstraintProvider<Coordinate, usize> for TileSet {
-    fn check(&self, a: Coordinate, av: &usize, b: Coordinate, bv: &usize) -> bool {
-        if let Some(dir) = a.is_adjacent(&b) {
-            self.relations[*av][get_relation_index(&dir)].contains(bv)
-        } else {
-            false
-        }
-    }
-}
+mod tile_matcher;
 
 fn build_arcs(x_lim: usize, y_lim: usize) -> Vec<(Coordinate, Coordinate)> {
     let mut arcs = vec![];
     for x in 0..x_lim {
         for y in 0..y_lim {
-            let base = Coordinate(x, y);
+            let base = Coordinate::new(x, y);
             if x < x_lim - 1 {
-                arcs.push((base, Coordinate(x + 1, y)));
-                arcs.push((Coordinate(x + 1, y), base));
+                arcs.push((base, Coordinate::new(x + 1, y)));
+                arcs.push((Coordinate::new(x + 1, y), base));
             }
             if y < y_lim - 1 {
-                arcs.push((base, Coordinate(x, y + 1)));
-                arcs.push((Coordinate(x, y + 1), base));
+                arcs.push((base, Coordinate::new(x, y + 1)));
+                arcs.push((Coordinate::new(x, y + 1), base));
             }
         }
     }
@@ -190,47 +26,116 @@ fn build_arcs(x_lim: usize, y_lim: usize) -> Vec<(Coordinate, Coordinate)> {
     arcs
 }
 
+struct BacktrackState<V, D> {
+    variable: V,
+    domains: HashMap<Coordinate, Vec<D>>,
+    untested: Vec<D>,
+}
+
 fn main() {
     let mut rng = simple_rng("hello world oasdf");
 
     let tiles = TileSet::new();
     let starting_domain = (0..tiles.tiles.len()).collect::<Vec<_>>();
-    let x_lim = 40;
-    let y_lim = 10;
+    let x_lim = 80;
+    let y_lim = 20;
     tiles.debug();
 
     let mut domains = HashMap::new();
     for x in 0..x_lim {
         for y in 0..y_lim {
-            domains.insert(Coordinate(x, y), starting_domain.clone());
+            domains.insert(Coordinate::new(x, y), starting_domain.clone());
         }
     }
 
     let arcs = build_arcs(x_lim, y_lim);
+    let mut backtrack: Vec<BacktrackState<Coordinate, usize>> = vec![];
 
     loop {
+        // First, make domains consistent.
         csp::ac3(&mut domains, &arcs, &tiles);
-        // find most constrained, reduce it, if there are any left to reduce.
-        if let Some((_, reducable)) = domains
+
+        // Second, search for the most constrained unsolved variable and try to choose a value for it.
+        if let Some(v) = domains
             .iter_mut()
             .filter(|(_, v)| v.len() > 1)
             .min_by(|a, b| a.1.len().cmp(&b.1.len()))
+            .map(|min| *min.0)
         {
+            let domains_clone = domains.clone();
+            // TODO: Unwrap :((
+            let reducable = domains.get_mut(&v).unwrap();
+            // Use a random index for selecting the answer, and store the selection.
             if let Some(selected) = reducable.choose(&mut rng).copied() {
+                println!("Selecting and recording {} (depth {})", v, backtrack.len());
+                backtrack.push(BacktrackState {
+                    variable: v,
+                    domains: domains_clone,
+                    untested: reducable
+                        .iter()
+                        .copied()
+                        .filter(|v| selected == *v)
+                        .collect(),
+                });
                 reducable.retain(|dv| *dv == selected);
             }
+
+            // print_domain_counts(&domains, y_lim, x_lim);
+        }
+        // Alternatively, if there are no viable selections to be made
+        else if domains.iter().any(|(_, tiles)| tiles.is_empty()) {
+            println!("Testing alternative..");
+            let Some(mut prev) = backtrack.pop() else {
+                panic!("No solution");
+            };
+
+            // Choose from the untested values, if there are any.
+            if let Some(selected) = prev.untested.choose(&mut rng).copied() {
+                prev.untested.retain(|v| *v != selected);
+                println!("Choosing {selected} leaving {:?}", prev.untested);
+                domains.clone_from(&prev.domains);
+                domains
+                    .entry(prev.variable)
+                    .and_modify(|d| d.retain(|v| *v == selected));
+                backtrack.push(prev);
+            }
+            // If there are no untested values, go back to the top of the backtrack and reset the domain.
+            else {
+                println!("Bubbling up");
+                domains.clone_from(&prev.domains);
+            }
         } else {
-            println!("no more reducing possible");
+            println!("All done.");
             break;
         }
     }
 
+    print_domains(&domains, y_lim, x_lim, tiles);
+}
+
+fn print_domain_counts(domains: &HashMap<Coordinate, Vec<usize>>, y_lim: usize, x_lim: usize) {
+    for y in (0..y_lim).rev() {
+        print!("{y:>3} ");
+        for x in 0..x_lim {
+            let c = domains[&Coordinate::new(x, y)].len();
+            print!("{c:>3}");
+        }
+        println!();
+    }
+}
+
+fn print_domains(
+    domains: &HashMap<Coordinate, Vec<usize>>,
+    y_lim: usize,
+    x_lim: usize,
+    tiles: TileSet,
+) {
     if domains.iter().all(|(_, tiles)| tiles.len() == 1) {
         println!("Result:");
         for y in (0..y_lim).rev() {
             print!("{y:>3} ");
             for x in 0..x_lim {
-                if let Some(v) = domains[&Coordinate(x, y)].first() {
+                if let Some(v) = domains[&Coordinate::new(x, y)].first() {
                     print!("{}", tiles.tiles[*v]);
                 } else {
                     print!("x");
@@ -245,24 +150,4 @@ fn main() {
 
 fn simple_rng(seed_str: &str) -> SmallRng {
     Seeder::from(seed_str).make_rng()
-}
-
-#[cfg(test)]
-mod test {
-    use crate::{Coordinate, Direction};
-
-    #[test]
-    fn coordinate_is_adjacent() {
-        let c0_0 = Coordinate(0, 0);
-        let c1_1 = Coordinate(1, 1);
-        let c0_1 = Coordinate(0, 1);
-        assert_eq!(c0_0.is_adjacent(&c1_1), None);
-        assert_eq!(c0_0.is_adjacent(&c0_1), Some(Direction::Up));
-        assert_eq!(c0_1.is_adjacent(&c0_0), Some(Direction::Down));
-        assert_eq!(c0_1.is_adjacent(&c1_1), Some(Direction::Right));
-        assert_eq!(c1_1.is_adjacent(&c0_1), Some(Direction::Left));
-    }
-
-    #[test]
-    fn generate() {}
 }
--- src/tile_matcher.rs
+++ src/tile_matcher.rs
@@ -1,100 +1,196 @@
-/*
-use std::collections::HashSet;
-use std::sync::Arc;
+use crate::ConstraintProvider;
+use core::fmt::Display;
 
-type ClonableBinaryConstraintFunction<D> = Arc<dyn Send + Sync + Fn(D, D) -> bool + 'static>;
 
-type TupleMatcher = ClonableBinaryConstraintFunction<i32>;
 
-pub fn tuple_matchers(a: i32, b: i32) -> TupleMatcher {
-    Arc::new(move |av: i32, bv: i32| av == a && bv == b)
+/// Direction tracks relationships between tiles.
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum Direction {
+    Down,
+    Left,
+    Right,
+    Up,
 }
 
-pub struct TileMatchBuilder {
-    up_down: HashSet<(i32, i32)>,
-    left_right: HashSet<(i32, i32)>,
+/// Define a position on the 2d map.
+/// Assume that **Down the screen** is lower Y -- The bottom of the screen y = 0.
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
+pub struct Coordinate(usize, usize);
+
+impl Coordinate {
+    /// Make a coordinate
+    pub fn new(x: usize, y: usize) -> Self {
+        Self(x, y)
+    }
+
+    /// Returns the direction `other` is in relation to `self`.
+    pub fn is_adjacent(&self, other: &Self) -> Option<Direction> {
+        let x_adjacent =
+            (self.0 > 0 && self.0 - 1 == other.0) || (self.0 < usize::MAX && self.0 + 1 == other.0);
+        let y_adjacent =
+            (self.1 > 0 && self.1 - 1 == other.1) || (self.1 < usize::MAX && self.1 + 1 == other.1);
+
+        if !(x_adjacent ^ y_adjacent) {
+            None
+        } else if self.0 < other.0 {
+            Some(Direction::Right)
+        } else if self.0 > other.0 {
+            Some(Direction::Left)
+        } else if self.1 < other.1 {
+            Some(Direction::Up)
+        } else {
+            Some(Direction::Down)
+        }
+    }
 }
 
-impl TileMatchBuilder {
+/// Collection of tiles and valid relationships to each other.
+pub struct TileSet {
+    /// Visual reference for what each tile is.
+    pub tiles: Vec<char>,
+
+    // Tile index relation to other tile indexes by direction. E.g. `tiles`
+    // index 0 is relations index 0, and has `[up, down, left, right]` where `up`
+    // is a vec of indexes into `tiles`.
+    pub relations: Vec<[Vec<usize>; 4]>,
+}
+
+fn new_relation(
+    up: Vec<usize>,
+    down: Vec<usize>,
+    left: Vec<usize>,
+    right: Vec<usize>,
+) -> [Vec<usize>; 4] {
+    [up, down, left, right]
+}
+
+impl Default for TileSet {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl TileSet {
     pub fn new() -> Self {
-        Self {
-            up_down: HashSet::new(),
-            left_right: HashSet::new(),
+        // Tiles:
+        // ░
+        // ┌─┐
+        // │▓│
+        // └─┘
+        //                           0    1    2    3    4    5    6    7
+        let tiles: Vec<char> = vec!['░', '┌', '─', '┐', '│', '▓', '└', '┘'];
+        // up down left right (where "up" means "this tile is below the provided one")
+        let relations = vec![
+            // 0
+            new_relation(
+                vec![0, 2, 6, 7],
+                vec![0, 1, 2, 3],
+                vec![0, 3, 4, 7],
+                vec![0, 1, 4, 6],
+            ),
+            // 1 ┌
+            new_relation(vec![0, 2, 6, 7], vec![4], vec![0, 4, 3, 7], vec![2]),
+            // 2 ─
+            new_relation(vec![0, 2, 5, 6, 7], vec![0, 1, 2, 3, 5,], vec![1, 6, 2], vec![2, 3, 7]),
+            // 3 ┐
+            new_relation(vec![0, 2, 6, 7], vec![4], vec![2], vec![0, 1, 4, 6]),
+            // 4 │
+            new_relation(vec![1, 3, 4], vec![4, 6, 7], vec![0, 5], vec![0, 5]),
+            // 5 ▓
+            new_relation(vec![2, 5], vec![2, 5], vec![4, 5], vec![4, 5]),
+            // 6 └
+            new_relation(vec![4], vec![0, 1, 2, 3], vec![0, 3, 4, 7], vec![2]),
+            // 7 ┘
+            new_relation(vec![4], vec![0, 1, 2, 3], vec![2], vec![]),
+        ];
+
+        Self { tiles, relations }
+    }
+
+    pub fn debug(&self) {
+        for (i, t) in self.tiles.iter().enumerate() {
+            let rels = &self.relations[i];
+            println!("Tile {i}: {t}");
+            let up = &rels[0];
+            let down = &rels[1];
+            let left = &rels[2];
+            let right = &rels[3];
+            for o in up.iter() {
+                println!(" up    {}", &self.tiles[*o]);
+                println!("       {t}");
+                println!("     xxx");
+            }
+            for o in down.iter() {
+                println!("       {t}");
+                println!(" down  {}", &self.tiles[*o]);
+                println!("     xxx");
+            }
+            for o in left.iter() {
+                println!(" left  {}{t}", &self.tiles[*o]);
+                println!("     xxx");
+            }
+            for o in right.iter() {
+                println!(" right {t}{}", &self.tiles[*o]);
+                println!("     xxx");
+            }
         }
     }
+}
 
-    pub fn left_right(mut self, l: i32, r: i32) -> Self {
-        self.left_right.insert((l, r));
-        self
-    }
-
-    pub fn up_down(mut self, u: i32, d: i32) -> Self {
-        self.up_down.insert((u, d));
-        self
-    }
-
-    pub fn build(self) -> TileMatchSet {
-        self.into()
+// up down left right
+fn get_relation_index(dir: Direction) -> usize {
+    match dir {
+        Direction::Up => 0,
+        Direction::Down => 1,
+        Direction::Left => 2,
+        Direction::Right => 3,
     }
 }
 
-pub struct TileMatchSet {
-    up: Vec<TupleMatcher>,
-    down: Vec<TupleMatcher>,
-    right: Vec<TupleMatcher>,
-    left: Vec<TupleMatcher>,
+impl Display for Coordinate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("({}, {})", self.0, self.1))
+    }
 }
 
-impl Into<TileMatchSet> for TileMatchBuilder {
-    fn into(self) -> TileMatchSet {
-        let up = self
-            .up_down
-            .iter()
-            .map(|(u, d)| tuple_matchers(*u, *d))
-            .collect();
-
-        let down = self
-            .up_down
-            .iter()
-            .map(|(u, d)| tuple_matchers(*d, *u))
-            .collect();
-
-        let left = self
-            .left_right
-            .iter()
-            .map(|(l, r)| tuple_matchers(*l, *r))
-            .collect();
-
-        let right = self
-            .left_right
-            .iter()
-            .map(|(l, r)| tuple_matchers(*r, *l))
-            .collect();
-
-        TileMatchSet {
-            up,
-            down,
-            right,
-            left,
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Direction::Up => f.write_str("up"),
+            Direction::Down => f.write_str("down"),
+            Direction::Left => f.write_str("left"),
+            Direction::Right => f.write_str("right"),
         }
     }
 }
 
-impl TileMatchSet {
-    pub fn left(&self) -> &Vec<TupleMatcher> {
-        &self.left
-    }
-
-    pub fn right(&self) -> &Vec<TupleMatcher> {
-        &self.right
-    }
-
-    pub fn up(&self) -> &Vec<TupleMatcher> {
-        &self.up
-    }
-
-    pub fn down(&self) -> &Vec<TupleMatcher> {
-        &self.down
+impl ConstraintProvider<Coordinate, usize> for TileSet {
+    fn check(&self, a: Coordinate, av: &usize, b: Coordinate, bv: &usize) -> bool {
+        if let Some(dir) = a.is_adjacent(&b) {
+            // TODO: Should consider making this safer.
+            self.relations[*av][get_relation_index(dir)].contains(bv)
+        } else {
+            false
+        }
     }
 }
- */
+
+#[cfg(test)]
+mod test {
+    use super::{Coordinate, Direction};
+
+    #[test]
+    fn coordinate_is_adjacent() {
+        let c0_0 = Coordinate(0, 0);
+        let c1_1 = Coordinate(1, 1);
+        let c0_1 = Coordinate(0, 1);
+        assert_eq!(c0_0.is_adjacent(&c1_1), None);
+        assert_eq!(c0_0.is_adjacent(&c0_1), Some(Direction::Up));
+        assert_eq!(c0_1.is_adjacent(&c0_0), Some(Direction::Down));
+        assert_eq!(c0_1.is_adjacent(&c1_1), Some(Direction::Right));
+        assert_eq!(c1_1.is_adjacent(&c0_1), Some(Direction::Left));
+    }
+
+    #[test]
+    fn generate() {}
+}
